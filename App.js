121aa2ce001c9066520539abf6054e15649029f1
Stanjhae <ceejayorji@yahoo.com>
2018-09-11 11:17:03 +0200

    Updated Deprecated Navigator to React-Navigation

--- App.js
+++ App.js
@@ -1,45 +1,19 @@
 import React, { Component } from 'react';
-import {
-  Navigator,    // Allows to navigate between different screens
-  StatusBar     // Allows to hide the status bar
-} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 import List from './List';
 import Movie from './Movie';
 
-const RouteMapper = (route, navigationOperations, onComponentRef) => {
-  if (route.name === 'list') {
-    return (
-      <List navigator={navigationOperations} />
-    );
-  } else if (route.name === 'movie') {
-    return (
-      <Movie
-        // Pass movie object passed with route down as a prop
-        movie={route.movie}
-        // Pass navigationOperations as navigator prop
-        navigator={navigationOperations}
-      />
-    );
-  }
-};
-
-export default class App extends Component {
-  componentDidMount() {
-    // Hide the status bar
-    StatusBar.setHidden(true);
-  }
-
-  render() {
-    return (
-      // Handle navigation between screens
-      <Navigator
-        // Default to list route
-        initialRoute={{name: 'list'}}
-        // Use FloatFromBottom transition between screens
-        configureScene={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
-        // Pass a route mapper functions
-        renderScene={RouteMapper}
-      />
-    );
-  }
-}
+export default createStackNavigator({
+    Home: {
+      screen: List,
+    },
+    Movie: {
+      screen: Movie,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      header: null,
+    },
+  });
